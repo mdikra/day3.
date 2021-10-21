e25762a99336bdd2a31df25f5a6db2f31216f19d
mdikra <91055842+mdikra@users.noreply.github.com>
2021-10-21 23:46:37 +0530

    Add files via upload

---
+++ codes/6Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace employeecheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+            const int EMP_RATE_PER_HOUR = 20;
+            const int N0_OF_WORKING_DAY = 20;
+            int empWage = 0;
+            int empHrs = 0;
+
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 3);
+            int day = 0;
+
+
+
+    
+
+        }
+    }
+}
